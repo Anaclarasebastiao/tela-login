266b286f39b036827705fdade91cff47bab7e20b
Formiga-aninha <anaclarasebastiao1716@gmail.com>
2024-10-24 15:14:22 -0300

    ajustes

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@ const email_input = document.getElementById('email-input')
 const password_input = document.getElementById('password-input')
 const error_message = document.getElementById('error-message')
 
-form.addEventListener('submit' , (e) => {
+form.addEventListener('submit', (e) => {
     e.preventDefault();
     alert(email_input.value);
     error_message.innerText = password_input.value;
